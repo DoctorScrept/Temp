563ef896ce841d2dade0ad4438c60ae3fc02ea95
DrScript7 <DrScript7@localhost>
2016-05-17 21:45:34 +0300

    Add Unity

---
+++ Unity/Assets/Core/USBDevice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class USBDevice : MonoBehaviour {
+	
+	protected IntPtr? hLib = null;
+
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	protected delegate int IntResultFunction();
+	
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	protected delegate int VoidFuntion();
+
+	protected void TryLoadDll(string path)
+	{
+		hLib = NativeMethods.LoadLibrary(path);
+		if (hLib.Value == IntPtr.Zero) {
+			hLib = null;
+		}
+	}
+	
+	protected void ClearDll()
+	{
+		if (hLib == null) {
+			return;
+		}
+		if (!NativeMethods.FreeLibrary(hLib.Value)) {
+
+		}
+		hLib = null;
+	}
+
+	protected Delegate LoadFunction<T>(IntPtr? hModule, string name) where T: class
+	{
+		if (hModule == null) {
+			return null;
+		}
+		IntPtr pAddressOfFunctionToCall = NativeMethods.GetProcAddress(hModule.Value, name);
+		if (pAddressOfFunctionToCall == IntPtr.Zero) {
+			ClearDll();
+			return null;
+		}
+		return Marshal.GetDelegateForFunctionPointer(pAddressOfFunctionToCall, typeof(T));
+	}
+
+	public bool IsReady() {
+		return hLib != null;
+	}
+}
---
+++ Unity/Assets/Step/StepUSBDevice.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StepUSBDevice : USBDevice
+{
+	public Text stateText;
+
+	IntResultFunction start;
+	IntResultFunction getMajorVersion;
+	IntResultFunction getMinorVersion;
+	IntResultFunction isVersionConfirmed;
+	
+	VoidFuntion left;
+	VoidFuntion right;
+	VoidFuntion stop;
+
+	void Awake()
+	{
+		TryLoadDll(@"E:\Diplom\TempDocs\Cpp\CurveTracer3DDriver\Debug\CurveTracer3DDriver.dll");
+		if (hLib == null) {
+			return;
+		}
+
+		start = LoadFunction<IntResultFunction>(hLib, "Start") as IntResultFunction;
+		getMajorVersion = LoadFunction<IntResultFunction>(hLib, "GetMajorVersion") as IntResultFunction;
+		getMinorVersion = LoadFunction<IntResultFunction>(hLib, "GetMinorVersion") as IntResultFunction;
+		isVersionConfirmed = LoadFunction<IntResultFunction>(hLib, "IsVersionConfirmed") as IntResultFunction;
+		left = LoadFunction<VoidFuntion>(hLib, "TurnLeft") as VoidFuntion;
+		right = LoadFunction<VoidFuntion>(hLib, "TurnRight") as VoidFuntion;
+		stop = LoadFunction<VoidFuntion>(hLib, "Stop") as VoidFuntion;
+
+		if (hLib == null) {
+			// One or more functions not found
+		}
+	}
+
+	void OnApplicationQuit() {
+		ClearDll();
+	}
+
+	public void Connect()
+	{
+		System.Text.StringBuilder builder = new System.Text.StringBuilder(100);
+		int result = start();
+		if (result == 0) {
+			builder.Append("Firmware v");
+			builder.Append(getMajorVersion());
+			builder.Append(".");
+			builder.Append(getMinorVersion());
+
+			if (isVersionConfirmed() != 0) {
+				builder.Append("\nFirmware version confirmed!");
+			} else {
+				builder.Append("\nFirmware version error!");
+
+			}
+
+			stateText.text = builder.ToString();
+		} else {
+			builder.Append("Error code: ");
+			builder.Append(result);
+		}
+	}
+
+	public void TurnLeft() {
+		left();
+	}
+
+	public void TurnRight() {
+		right();
+	}
+
+	public void Stop() {
+		stop();
+	}
+}
